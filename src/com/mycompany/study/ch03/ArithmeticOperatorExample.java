ebbf7bc00c18e2cd823e21fd820b050c964da1b8
kik7511 <kik7511@naver.com>
2022-08-02 16:20:43 +0900

    0802

---
+++ src/com/mycompany/study/ch03/ArithmeticOperatorExample.java
@@ -0,0 +1,26 @@
+package com.mycompany.study.ch03;
+
+public class ArithmeticOperatorExample {
+
+	public static void main(String[] args) {
+		
+		int v1=5;
+		int v2=2;
+		
+		int result1=v1+v2;
+		int result2=v1-v2;
+		int result3=v1*v2;
+		int result4=v1/v2;
+		int result5=v1%v2;
+		double result6=v1/v2;
+		double result7=(double)v1/v2;
+		System.out.println("result1: " + result1);
+		System.out.println("result2: " + result2);
+		System.out.println("result3: " + result3);
+		System.out.println("result4: " + result4);
+		System.out.println("result5: " + result5);
+		System.out.println("result6: " + result6);
+		System.out.println("result7: " + result7);
+	}
+
+}
--- src/com/mycompany/study/ch03/DenyLogicoperatorExample.java
+++ src/com/mycompany/study/ch03/DenyLogicoperatorExample.java
@@ -3,6 +3,22 @@ package com.mycompany.study.ch03;
 public class DenyLogicoperatorExample {
 
 	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println("play: " + play);
+		 
+		play= !play;
+		System.out.println("play: " + play);
+		
+		play= !play;
+		System.out.println("play: " + play);
+		
+		int j=3;
+		
+		if(!(j==3)) {
+			System.out.println("3이 아님");
+		}else {
+			System.out.println("3임");
+		}
 		
 	}
 
