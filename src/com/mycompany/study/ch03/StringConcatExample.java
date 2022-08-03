f167e593635d67d2f571308ce963e5e34c1045a6
kik7511 <kik7511@naver.com>
2022-08-03 17:15:02 +0900

    0803

--- src/com/mycompany/study/ch03/StringConcatExample.java
+++ src/com/mycompany/study/ch03/StringConcatExample.java
@@ -3,7 +3,9 @@ package com.mycompany.study.ch03;
 public class StringConcatExample {
 
 	public static void main(String[] args) {
-		
+		String tool="jdk";
+		String version="1.11";
+		System.out.println("우리가 사용하는 도구는 "+tool+" "+version+"입니다.");
 	}
 
 }
