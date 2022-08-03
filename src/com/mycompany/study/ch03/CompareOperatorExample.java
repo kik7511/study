5d417524ce08b759a8ce797ef1217ed8672f723b
kik7511 <kik7511@naver.com>
2022-08-03 17:26:22 +0900

    0803

--- src/com/mycompany/study/ch03/CompareOperatorExample.java
+++ src/com/mycompany/study/ch03/CompareOperatorExample.java
@@ -3,8 +3,22 @@ package com.mycompany.study.ch03;
 public class CompareOperatorExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a= 10;
+		int b=20;
+		boolean result= (a==b);
+		boolean result2= (a!=b);
+		boolean result3= (a<b);
+		boolean result4= (a<=b);
+		boolean result5= (a>b);
+		boolean result6= (a>=b);
+		
+		
+		System.out.println(result);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+		System.out.println(result6);
 	}
 
 }
