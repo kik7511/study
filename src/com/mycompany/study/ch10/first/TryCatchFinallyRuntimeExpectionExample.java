9c9159e4a50dc1be4a0ee4a0c63a8ef6f91057af
kik7511 <kik7511@naver.com>
2022-09-29 16:49:00 +0900

    0929

--- src/com/mycompany/study/ch10/first/TryCatchFinallyRuntimeExpectionExample.java
+++ src/com/mycompany/study/ch10/first/TryCatchFinallyRuntimeExpectionExample.java
@@ -16,15 +16,12 @@ public class TryCatchFinallyRuntimeExpectionExample {
 			return;
 		}
 		
-		try {
-			int value1 = Integer.parseInt(data1);
-			int value2 = Integer.parseInt(data2);
-			int result = value1 + value2;
-			System.out.println(data1 +  "+" + data2 + "=" + result);
-		}catch(NumberFormatException e){
-			System.out.println("숫자로 변환 할 수 없습니다.");
-		}finally {
-			System.out.println("다시 실행하세요.");
-		}
+		
+		  try { int value1 = Integer.parseInt(data1); int value2 =
+		  Integer.parseInt(data2); int result = value1 + value2;
+		  System.out.println(data1 + "+" + data2 + "=" + result);
+		  }catch(NumberFormatException e){ System.out.println("숫자로 변환 할 수 없습니다.");
+		  }finally { System.out.println("다시 실행하세요."); }
+		 
 	}
 }
